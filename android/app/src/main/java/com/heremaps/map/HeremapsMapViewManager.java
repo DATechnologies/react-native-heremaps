ac96f480c686fb185b19f4a04759f152992c11a5
zhhjoseph <zhhjoseph@gmail.com>
2020-02-19 13:35:46 -0500

    build.gradle created

---
+++ android/app/src/main/java/com/heremaps/map/HeremapsMapViewManager.java
@@ -0,0 +1,28 @@
+package com.heremaps.map;
+
+import android.app.Activity;
+
+import com.facebook.react.bridge.ReadableMap;
+import com.facebook.react.common.MapBuilder;
+import com.facebook.react.uimanager.SimpleViewManager;
+import com.facebook.react.uimanager.ThemedReactContext;
+import com.facebook.react.uimanager.annotations.ReactProp;
+import com.here.android.mpa.common.ApplicationContext;
+
+import java.util.Map;
+
+public class HeremapsMapViewManager extends SimpleViewManager {
+
+  private static final String REACT_CLASS = "HereMapsAndroidMapView";
+
+  @Override
+  public String getName() {
+    return REACT_CLASS;
+  }
+
+  @Override
+  protected HeremapsMapView createViewInstance(ThemedReactContext context) {
+    return new HeremapsMapView(context);
+  }
+
+}
